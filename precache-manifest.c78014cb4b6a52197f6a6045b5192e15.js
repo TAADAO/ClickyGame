069bef3f210e41266070e67b6eb6faf7c2570d99
Robert O <romohund@gmail.com>
2019-04-15 09:46:32 -0500

    Updates

---
+++ precache-manifest.c78014cb4b6a52197f6a6045b5192e15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  },
+  {
+    "revision": "47b03803c1f577bc8833",
+    "url": "/ClickyGame/static/js/main.91aa9fc3.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ClickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "47b03803c1f577bc8833",
+    "url": "/ClickyGame/static/css/main.03fcf31f.chunk.css"
+  },
+  {
+    "revision": "2f7eb834d87719c91995046a971b9fd6",
+    "url": "/ClickyGame/index.html"
+  }
+];
